491c0240a35183955b91137c0afcc805e0ceaf24
Kulin Rangani <kulinrangani01@gmail.com>
2022-09-04 17:15:07 +0530

    Added condition for .

--- src/App.js
+++ src/App.js
@@ -30,8 +30,11 @@ function reducer(state, { type, payload }) {
       if (payload.digit === "0" && state.currentOperand === "0") {
         return state;
       }
-      if (payload.digit === "." && state.currentOperand.includes(".")) {
-        return state;
+      if (payload.digit === "." || state.currentOperand.includes(".")) {
+        return {
+          ...state,
+          currentOperand: "0.",
+        };
       }
 
       return {
